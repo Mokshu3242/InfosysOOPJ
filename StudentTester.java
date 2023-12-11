99d0935cb5e71f8f4879f2005109db07039b2f4d
Mokshad Ketan Sankhe <72201878+Mokshu3242@users.noreply.github.com>
2023-12-11 11:55:40 +0530

    Update StudentTester.java

--- StudentTester.java
+++ StudentTester.java
@@ -1,28 +1,43 @@
 public class StudentTester {
-    private int studentID = 1001;
+    private int studentID;
     public int getStudentID() {
         return studentID;
     }
+    public void setStudentID(int studentID){
+        this.studentID = studentID;
+    }
 
-    private String name = "jacob";
+    private String name;
     public String getName() {
         return name;
     }
+    public void setName(String name){
+        this.name = name;
+    }
 
-    private float qualifyingMarks = 80;
+    private float qualifyingMarks;
     public float getQualifyingMarks() {
         return qualifyingMarks;
     }
+    public void setQualifyingMarks(float qualifyingMarks){
+        this.qualifyingMarks = qualifyingMarks;
+    }
 
-    private char residentialStatus = 'H';
+    private char residentialStatus;
     public char getResidentialStatus() {
         return residentialStatus;
     }
+    public void setResidentialStatus(char residenceStatus){
+        this.residentialStatus = residenceStatus;
+    }    
 
-   private int yearOfEng = 3;
+    private int yearOfEng;
     public int getYearOfEng() {
         return yearOfEng;
     } 
+    public void setYearOfEng(int yearOfEng){
+        this.yearOfEng = yearOfEng;
+    }
 
     public static void main(String[] args) {
         StudentTester student = new StudentTester();
@@ -31,6 +46,11 @@ public class StudentTester {
         float qualifiedMarks = student.getQualifyingMarks();
         char residenceStatus = student.getResidentialStatus();
         int yearOf = student.getYearOfEng();
+        student.setName("jacob");
+        student.setStudentID(1001);
+        student.setQualifyingMarks(80);
+        student.setResidentialStatus('H');
+        student.setYearOfEng(3);
 
         System.out.println("Student ID: " + id);
         System.out.println("Student Name: " + name);
